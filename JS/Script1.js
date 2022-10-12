2b6964cf3a789e17402320007cfde90d5ba67d41
Edson Comini Clemente Lopes <edcomini@gmail.com>
2022-10-11 22:28:15 -0300

    Initial commit

---
+++ JS/Script1.js
@@ -0,0 +1,23 @@
+class Motocicleta{
+
+    constructor(){
+        this.sku = 0;
+        this.marca = '';
+        this.modelo = '';
+        this.anoFabricacao = '';
+        this.valor = 0;
+    }
+
+    adicionar(){
+        alert('Vamos adicionar uma motocicleta');
+        //...
+    }
+
+    excluir(){
+        //...
+        alert('item deletado');
+    }
+}
+
+
+var motocicleta = new Motocicleta();
---
+++ README.md
@@ -0,0 +1,2 @@
+# projeto_POO_CRUD_JavaScript
+ Conceito de POO usando JavaScript na criação de CRUD
